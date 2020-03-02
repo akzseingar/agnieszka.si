0af1f1b99ac7e6e7ce428511e81d34108a51a0ec
akzseingar <gigiagnieszka@gmail.com>
2020-03-02 23:12:17 +0100

    auto push

--- posodobi.sh
+++ posodobi.sh
@@ -2,20 +2,6 @@
 set -e -x
 pwd
 
-# make a new version of the homepage
-hugo
-
-# go to publish folder
-cd ../akzseingar.github.io
-ls
-
-# delete old files
-rm -r about categories contact css img js portfolio sitemap.xml tags || true
-cp -r ../agnieszka2017/public/* .
-
-touch .nojekyll
-
-# comming and update
 git add .
 git commit -a -m "auto push" || true # can fail on no changes
 git push
